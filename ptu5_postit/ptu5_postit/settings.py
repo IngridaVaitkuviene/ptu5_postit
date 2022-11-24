6a3207693fe569a3a0fc96c8a2e75c9dd9f17c6c
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-24 14:25:50 +0200

    ištrintas susidubliavęs importas

--- ptu5_postit/ptu5_postit/settings.py
+++ ptu5_postit/ptu5_postit/settings.py
@@ -15,8 +15,6 @@ from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-from . import local_settings
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
