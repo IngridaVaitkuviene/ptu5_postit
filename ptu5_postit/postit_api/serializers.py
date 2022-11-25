81ec9cca76a52d55e82adf8a566140772c3785e8
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-25 13:54:21 +0200

    comment added

--- ptu5_postit/postit_api/serializers.py
+++ ptu5_postit/postit_api/serializers.py
@@ -2,9 +2,27 @@ from rest_framework import serializers
 from . import models
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    post = serializers.ReadOnlyField(source='post.id')
+
+    class Meta:
+        model = models.Comment
+        fields = ('id', 'post', 'body', 'user', 'user_id', 'created_at')
+
+
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     user_id = serializers.ReadOnlyField(source='user.id')
+    comments = CommentSerializer(many=True)
+    # comments = serializers.StringRelatedField(many=True)
+    comments_count = serializers.SerializerMethodField()
+
+    def get_comments_count(self, obj):
+        return models.Comment.objects.filter(post=obj).count()
+
+
     class Meta:
         model = models.Post
-        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at')
+        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at', 'comments', 'comments_count')
